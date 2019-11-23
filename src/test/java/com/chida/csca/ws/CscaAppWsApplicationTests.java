80131e8423d12fee24c1341166eee0bd1f6be04f
chids_p <chids123>
2019-11-23 09:23:05 +0530

    Initial project setup

---
+++ src/test/java/com/chida/csca/ws/CscaAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chida.csca.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscaAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
